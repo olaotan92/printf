4468a36446a41670416a6010b483f2b78bd02247
olaotan ojo <elijahshawntel@gmail.com>
2022-10-21 17:56:50 -0700

    functions1

--- main.h
+++ main.h
@@ -103,7 +103,7 @@ int write_pointer(char buffer[], int ind, int length,
 	int width, int flags, char padd, char extra_c, int padd_start);
 
 int write_unsgnd(int is_negative, int ind,
-char buffer,
+char buffer[],
 	int flags, int width, int precision, int size);
 
 /****************** UTILS *******************/
